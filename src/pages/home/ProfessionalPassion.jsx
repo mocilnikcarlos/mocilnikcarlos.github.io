11e47f6d13ab5b1549e12ee599ca99bcfe2bc061
Carlos Mocilnik <carlos.mocilnik@hotmail.com>
2022-05-17 19:51:12 -0300

    Añadido hook useRef a las secciones home

--- src/pages/home/ProfessionalPassion.jsx
+++ src/pages/home/ProfessionalPassion.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import { CardProfessionalPassionUp } from "../../components/cards/CardProfessionalPassion";
 import { TitleWeb1 } from "../../components/pages/TitleWeb";
 import dataProfessional from "../../json/cardProfessionalPassion.json";
+import useRefScroll from "../../hooks/useScrollShow";
 import "./ProfessionalPassion.scss";
 
 const ProfessionalPassion = () => {
+  const { refScroll, elementIsVisible } = useRefScroll();
+
   const dataUp = dataProfessional.getProfessional[0];
   const dataDown = dataProfessional.getProfessional[1];
 
@@ -12,7 +15,12 @@ const ProfessionalPassion = () => {
     <section className="section-professionalpassion">
       <article className="article-professionalpassion">
         <TitleWeb1 titleweb="Pasión profesional" />
-        <div className="box-cardsprofessionalpassion">
+        <div
+          ref={refScroll}
+          className={`box-cardsprofessionalpassion hide ${
+            elementIsVisible && "show"
+          }`}
+        >
           <div className="boxcards">
             {dataUp.dataProfessionalUp.map((el) => (
               <CardProfessionalPassionUp key={el.id} Professional={el} />
--- src/pages/home/UserExperience.jsx
+++ src/pages/home/UserExperience.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import CardUserExperience from "../../components/cards/CardUserExperience";
 import { TitleWeb2 } from "../../components/pages/TitleWeb";
 import dataCardUx from "../../json/cardUserExperience.json";
+import useRefScroll from "../../hooks/useScrollShow";
 import "./UserExperience.scss";
 
 const UserExperience = () => {
+  const { refScroll, elementIsVisible } = useRefScroll();
   return (
     <section className="section-userexperience">
       <article className="article-userexperience">
         <TitleWeb2 titleweb="Mejorando la experiencia del usuario" />
-        <div className="box-cardsuserexperience">
+        <div
+          ref={refScroll}
+          className={`box-cardsuserexperience hide ${
+            elementIsVisible && "show"
+          }`}
+        >
           {dataCardUx.dataCardUx.map((el) => (
             <CardUserExperience key={el.id} CardUx={el} />
           ))}
