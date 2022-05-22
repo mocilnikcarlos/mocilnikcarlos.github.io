64e1cf9229c029258df77aeb15743497c3a1dd7a
Carlos Mocilnik <carlos.mocilnik@hotmail.com>
2022-05-22 16:14:58 -0300

    Fix, top page when press F5 and scroll top in press link navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import Footer from "./components/footer/Footer";
 import Header from "./components/header/Header";
@@ -7,6 +7,12 @@ import Service from "./pages/service/Service";
 import "./sass/Style.scss";
 
 const App = () => {
+  useEffect(() => {
+    window.addEventListener("beforeunload", () => window.scrollTo(0, 0));
+    return () => {
+      window.removeEventListener("beforeunload", () => window.scrollTo(0, 0));
+    };
+  }, []);
   return (
     <Fragment>
       <Header />
--- src/components/header/LinkNavbar.jsx
+++ src/components/header/LinkNavbar.jsx
@@ -3,9 +3,17 @@ import { Link } from "react-router-dom";
 import "./LinkNavbar.scss";
 
 const ListNavbar = (props) => {
+  function refreshPage() {
+    window.scrollTo(0, 0);
+  }
+
   return (
     <li>
-      <Link to={props.PropertyLink.url} className="linknav">
+      <Link
+        to={props.PropertyLink.url}
+        onClick={refreshPage}
+        className="linknav"
+      >
         <span className="mask-navbar">
           <div className="link-container">
             <span className="link-visible block-fx">
