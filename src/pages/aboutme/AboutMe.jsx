c63355f25b7b7717c90c1656db3aad0e5c541ca2
Carlos Mocilnik <carlos.mocilnik@hotmail.com>
2022-05-24 19:18:10 -0300

    Agregada seccion Sobre Mi

---
+++ src/pages/aboutme/AboutMe.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AboutMe.scss";
+
+const AboutMe = () => {
+  return (
+    <section className="aboutme">
+      <article className="article-aboutme"></article>
+    </section>
+  );
+};
+
+export default AboutMe;
