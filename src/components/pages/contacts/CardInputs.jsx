781b44d24cb67f76d624aaa01ca9f82e8317ccf1
Carlos Mocilnik <carlos.mocilnik@hotmail.com>
2022-05-28 16:10:25 -0300

    Agregado seccion contacto

---
+++ src/components/pages/contacts/CardInputs.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./CardInputs.scss";
+
+const CardInputs = () => {
+  return (
+    <div className="box-card-inputs">
+      <div className="circlered"></div>
+      <div className="card-inputs"></div>
+    </div>
+  );
+};
+
+export default CardInputs;
